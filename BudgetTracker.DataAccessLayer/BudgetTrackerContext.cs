40ee1298624feec641d4fe729544806c5f1ca7cf
emrullahyabasun <yabasunemrullah@gmail.com>
2024-12-04 00:51:17 +0300

    Entities and DAL developments

---
+++ BudgetTracker.DataAccessLayer/BudgetTrackerContext.cs
@@ -0,0 +1,72 @@
+﻿using BudgetTracker.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BudgetTracker.DataAccessLayer
+{
+    public class BudgetTrackerContext : DbContext
+    {
+        public BudgetTrackerContext(DbContextOptions<BudgetTrackerContext> options) : base(options) { }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Income> Incomes { get; set; }
+        public DbSet<Expense> Expenses { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<PaymentMethod> PaymentMethods { get; set; }
+
+        /// <summary>
+        /// OnModelCreating metodu, veritabanı ilişkilerini ve konfigürasyonları ayarlamak için kullanılır
+        /// </summary>
+        /// <param name="modelBuilder"></param>
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            // User - Income: One-to-Many
+            modelBuilder.Entity<Income>()
+                .HasOne(i => i.User)
+                .WithMany(u => u.Incomes)
+                .HasForeignKey(i => i.UserId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            // User - Expense: One-to-Many
+            modelBuilder.Entity<Expense>()
+                .HasOne(e => e.User)
+                .WithMany(u => u.Expenses)
+                .HasForeignKey(e => e.UserId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            // Category - Income: One-to-Many
+            modelBuilder.Entity<Income>()
+                .HasOne(i => i.Category)
+                .WithMany(c => c.Incomes)
+                .HasForeignKey(i => i.CategoryId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            // Category - Expense: One-to-Many
+            modelBuilder.Entity<Expense>()
+                .HasOne(e => e.Category)
+                .WithMany(c => c.Expenses)
+                .HasForeignKey(e => e.CategoryId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            // PaymentMethod - Income: One-to-Many
+            modelBuilder.Entity<Income>()
+                .HasOne(i => i.PaymentMethod)
+                .WithMany(p => p.Incomes)
+                .HasForeignKey(i => i.PaymentMethodId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            // PaymentMethod - Expense: One-to-Many
+            modelBuilder.Entity<Expense>()
+                .HasOne(e => e.PaymentMethod)
+                .WithMany(p => p.Expenses)
+                .HasForeignKey(e => e.PaymentMethodId)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
+    }
+}
