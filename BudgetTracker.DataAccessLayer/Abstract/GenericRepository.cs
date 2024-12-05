9a8b658865dfd5ae22049de209d1fa551f6cf416
emrullahyabasun <yabasunemrullah@gmail.com>
2024-12-05 22:13:33 +0300

    DAL GenericRepository

---
+++ BudgetTracker.DataAccessLayer/Abstract/GenericRepository.cs
@@ -0,0 +1,53 @@
+﻿using BudgetTracker.DataAccessLayer.Interface;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BudgetTracker.DataAccessLayer.Abstract
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        private readonly BudgetTrackerContext _context;
+        private readonly DbSet<T> _dbSet;
+
+        public GenericRepository(BudgetTrackerContext context)
+        {
+            _context = context;
+            _dbSet = context.Set<T>();
+        }
+
+
+        public async Task AddAsync(T entity)
+        {
+            await _dbSet.AddAsync(entity);
+        }
+
+        public void Delete(T entity)
+        {
+            _dbSet.Remove(entity);
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync()
+        {
+            return await _dbSet.ToListAsync();
+        }
+
+        public async Task<T> GetByIdAsync(int id)
+        {
+            return await _dbSet.FindAsync(id);
+        }
+
+        public async Task SaveChangesAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+
+        public void Update(T entity)
+        {
+            _dbSet.Update(entity);
+        }
+    }
+}
