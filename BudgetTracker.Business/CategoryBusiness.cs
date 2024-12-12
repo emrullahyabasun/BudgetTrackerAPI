61209a0024f79d4b98e1ece069f752bf9aae3442
emrullahyabasun <yabasunemrullah@gmail.com>
2024-12-13 00:10:54 +0300

    emptied out for rewrite

--- BudgetTracker.Business/CategoryBusiness.cs
+++ BudgetTracker.Business/CategoryBusiness.cs
@@ -5,7 +5,5 @@ namespace BudgetTracker.Business
 {
     public class CategoryBusiness
     {
-
-       
     }
 }
--- BudgetTracker.Services/CategoryService.cs
+++ BudgetTracker.Services/CategoryService.cs
@@ -6,32 +6,5 @@ namespace BudgetTracker.Services
     public class CategoryService
     {
         private readonly CategoryBusiness _categoryBusiness;
-
-        public CategoryService(CategoryBusiness categoryBusiness)
-        {
-            _categoryBusiness = categoryBusiness;
-        }
-
-        public async Task<List<Category>> GetAllCategoriesAsync()
-        {
-            return await _categoryBusiness.GetAllCategoriesAsync();
-        }
-
-        public async Task<Category> GetCategoryByIdAsync(int id)
-        {
-            return await _categoryBusiness.GetCategoryByIdAsync(id);
-        }
-        public async Task AddCategoryAsync(Category category)
-        {
-            await _categoryBusiness.AddCategoryAsync(category);
-        }
-        public async Task UpdateCategoryAsync(Category category)
-        {
-            await _categoryBusiness.UpdateCategoryAsync(category);
-        }
-        public async Task DeleteCategoryByIdAsync(int id)
-        {
-            await _categoryBusiness.DeleteCategoryByIdAsync(id);
-        }
     }
 }
